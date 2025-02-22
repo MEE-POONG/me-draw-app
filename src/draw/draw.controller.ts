b54739e476b13167b677423d718f45a4f021d383
warayut teakrathok <warayut2555@gmail.com>
2025-02-22 13:37:34 +0700

    me guild

--- src/draw/draw.controller.ts
+++ src/draw/draw.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { DrawService } from './draw.service';
+import { GuildMember } from 'discord.js';
 
 @Controller('draw')
 export class DrawController {
   constructor(private readonly drawService: DrawService) {}
 
-  @Get('image-me-guild-welcome')
+  @Get('image')
   getImage(@Res() res: Response) {
     const imageBuffer = this.drawService.generateImage();
+    res.setHeader('Content-Type', 'image/png');
+    res.send(imageBuffer);
+  }
+
+  @Post('image-me-guild-welcome')
+  async getImageMeGuildWelcome(
+    @Res() res: Response,
+    @Body() member: GuildMember,
+  ) {
+    const imageBuffer =
+      await this.drawService.generateImageMeGuildWelcome(member);
     res.setHeader('Content-Type', 'image/png');
     res.send(imageBuffer);
   }
--- src/draw/draw.service.ts
+++ src/draw/draw.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { createCanvas, registerFont } from 'canvas';
+import { createCanvas, loadImage, registerFont } from 'canvas';
+import { GuildMember } from 'discord.js';
 import * as path from 'path';
 
 @Injectable()
 export class DrawService {
   constructor() {
-    // Register the Chonburi font
-    const fontPath = path.join(__dirname, '../assets/Chonburi-Regular.ttf');
+    // Register the Sriracha font
+    const fontPath = path.join(__dirname, '../assets/Sriracha-Regular.ttf');
     console.log('Font path:', fontPath);
     registerFont(fontPath, {
-      family: 'Chonburi',
+      family: 'Sriracha',
     });
   }
 
@@ -33,10 +34,45 @@ export class DrawService {
     ctx.arc(300, 200, 75, 0, Math.PI * 2);
     ctx.fill();
 
-    // Add text with Chonburi font
+    // Add text with Sriracha font
     ctx.fillStyle = '#000000';
-    ctx.font = '30px Chonburi';
-    ctx.fillText('Hello Canvas!', 100, 400);
+    ctx.font = '30px Sriracha';
+    ctx.fillText('Hello World สวัสดีชาวโลก', 100, 400);
+
+    // Return image as Buffer
+    return canvas.toBuffer('image/png');
+  }
+
+  async generateImageMeGuildWelcome(member: GuildMember): Promise<Buffer> {
+    const width = 500;
+    const height = 500;
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+
+    // Background color
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(0, 0, width, height);
+
+    // Draw a rectangle
+    ctx.fillStyle = '#FF5733';
+    ctx.fillRect(50, 50, 200, 150);
+
+    // Draw a circle
+    ctx.beginPath();
+    ctx.arc(300, 200, 75, 0, Math.PI * 2);
+    ctx.fill();
+
+    // โหลดภาพโปรไฟล์
+    const innerImage = await loadImage(
+      member.user.displayAvatarURL({ extension: 'png' }),
+    );
+    ctx.drawImage(innerImage, 100, 100, 100, 100);
+    ctx.restore();
+
+    // Add text with Sriracha font
+    ctx.fillStyle = '#000000';
+    ctx.font = '30px Sriracha';
+    ctx.fillText(`Hello ${member.user.username}`, 100, 400);
 
     // Return image as Buffer
     return canvas.toBuffer('image/png');
